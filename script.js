3d139e3d9c97276772818fc1a2b7187f56f5bdcd
sang nguyen <sang95200@gmail.com>
2020-10-19 20:43:00 +0700

    ok

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const googleDatabase = [
     'myfavoritecats2.com',
 ]
 
-const googleSearch = (searchInput) => {
-    const matches = googleDatabase.filter(website => {
+const googleSearch = (searchInput,db) => {
+    const matches = db.filter(website => {
         return website.includes(searchInput)
     })
     return matches.length > 3 ? matches.slice(0, 3) : matches
--- script.test.js
+++ script.test.js
@@ -1,5 +1,12 @@
 const googleSearch = require("./script")
 
+const dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'dogpictures.com'
+]
+
 it("this is a test ",()=>{
-    console.log(googleSearch);
+    console.log(googleSearch('dog',dbMock));
 })
