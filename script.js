c97c8178028decc9766f7c9a9dd95a06a3199a78
hao pham <sangthanh@carp.vn>
2020-10-19 18:11:04 +0700

    ok

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ const googleDatabase = [
     'myfavoritecats2.com',
 ]
 
-const googleSearch = (searchInput) =>{
-    const matches = googleDatabase.filter(website =>{
+const googleSearch = (searchInput) => {
+    const matches = googleDatabase.filter(website => {
         return website.includes(searchInput)
     })
-    return matches.length > 3 ? matches.slice(0,3) : matches
+    return matches.length > 3 ? matches.slice(0, 3) : matches
 }
-console.log(googleSearch("cats"))
+
+module.exports = googleSearch
