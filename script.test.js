c39f956ed0293b87783d5c294aa08dedbf0bd090
hao pham <sangthanh@carp.vn>
2020-10-21 10:45:37 +0700

    test asynchonoust

--- script.test.js
+++ script.test.js
@@ -7,6 +7,22 @@ const dbMock = [
     'dogpictures.com'
 ]
 
-it("this is a test ",()=>{
-    console.log(googleSearch('dog',dbMock));
+describe("google Search", () => {
+    it("is a silly test", () => {
+        expect("hello").toBe("hello")
+    })
+
+    it("is searching google ", () => {
+        expect(googleSearch('test', dbMock)).toEqual([])
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+    })
+
+    it("check null and undefined", () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([])
+        expect(googleSearch(null, dbMock)).toEqual([])
+    })
+
+    it("does not return more than 3 matches", () => {
+        expect(googleSearch(".com", dbMock).length).toEqual(3)
+    })
 })
